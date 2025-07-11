f78ab7e385971320055790dcb9b9a21507a51738
Julho César Moreira <julhomoreira1988@gmail.com>
2025-07-11 21:44:04 +0100

    update node
    update node

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+const server = express();
+
+server.get('/usuario', (req, res) => {
+    return res.send("Ésse é o seu usuário")
+})
+
+server.get('/usuario2', (req, res) => {
+    return res.send("Ésse é o seu usuário 2")
+})
+
+server.listen(3000)
